d52cd8cb5bbfc52f187964187fc530f416f162bb
tofim <tofim@gmail.com>
2026-03-07 23:47:26 +0500

    fix: correct default value for BOT_USERNAME in config

--- apps/api/src/config.ts
+++ apps/api/src/config.ts
@@ -3,7 +3,7 @@ import { getEnv } from "@codemit/shared/utils";
 const configs = {
   PORT: Number(getEnv("PORT", "4400")),
   BOT_TOKEN: getEnv("BOT_TOKEN"),
-  BOT_USERNAME: getEnv("BOT_USERNAME", "codemit_bot"),
+  BOT_USERNAME: getEnv("BOT_USERNAME", "codemitbot"),
   DASHBOARD_URL: getEnv("DASHBOARD_URL", "http://localhost:3000"),
   GOOGLE_CLIENT_ID: getEnv("GOOGLE_CLIENT_ID", ""),
   DEVS_CHAT_ID: getEnv("DEVS_CHAT_ID").split("/").map(Number),
